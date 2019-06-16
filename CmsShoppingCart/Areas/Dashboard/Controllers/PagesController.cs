7c449bb91dbabc0258ab1bcf6d4fe2d4a45cf226
ritesh <ritesh4714@gmail.com>
2019-06-16 13:38:43 +0530

    007

---
+++ CmsShoppingCart/Areas/Dashboard/Controllers/PagesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CmsShoppingCart.Areas.Dashboard.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Dashboard/Pages
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
